c4c09794512b4994f79c944d97303928b52e8568
KajaBraz <kajabraz@gmail.com>
2020-12-20 03:56:37 +0100

    handling 5 dimentions

--- day17.py
+++ day17.py
@@ -14,6 +14,11 @@ def get_initial_active_coordinates_4D(l: [[str]]) -> {(int)}:
     return set(active)
 
 
+def get_initial_active_coordinates_5D(l: [[str]]) -> {(int)}:
+    active = [(x, y, 0, 0, 0) for x in range(len(l)) for y in range(len(l[0])) if l[x][y] == '#']
+    return set(active)
+
+
 def get_neighbours_3D(point: ()) -> {(int)}:
     neighbours = set()
     for coor_x in [point[0] - 1, point[0], point[0] + 1]:
@@ -31,6 +36,18 @@ def get_neighbours_4D(point: ()) -> {(int)}:
             for coor_z in [point[2] - 1, point[2], point[2] + 1]:
                 for coor_w in [point[3] - 1, point[3], point[3] + 1]:
                     neighbours.add((coor_x, coor_y, coor_z, coor_w))
+    neighbours.remove(point)
+    return neighbours
+
+
+def get_neighbours_5D(point: ()) -> {(int)}:
+    neighbours = set()
+    for coor_x in [point[0] - 1, point[0], point[0] + 1]:
+        for coor_y in [point[1] - 1, point[1], point[1] + 1]:
+            for coor_z in [point[2] - 1, point[2], point[2] + 1]:
+                for coor_w in [point[3] - 1, point[3], point[3] + 1]:
+                    for coor_q in [point[4] - 1, point[4], point[4] + 1]:
+                        neighbours.add((coor_x, coor_y, coor_z, coor_w, coor_q))
     neighbours.remove(point)
     return neighbours
 
@@ -57,5 +74,6 @@ def check_grid(initial_grid: [[str]], turns: int, function_neighbours_dimension,
 
 if __name__ == '__main__':
     input = get_list('input17.txt')
-    print(check_grid(input, 6, get_neighbours_3D,get_initial_active_coordinates_3D))
-    print(check_grid(input, 6, get_neighbours_4D,get_initial_active_coordinates_4D))
+    print(check_grid(input, 6, get_neighbours_3D, get_initial_active_coordinates_3D))  # part 1 solution
+    print(check_grid(input, 6, get_neighbours_4D, get_initial_active_coordinates_4D))  # part 2 solution
+    print(check_grid(input, 6, get_neighbours_5D, get_initial_active_coordinates_5D))  # extra solution for 5 dimentions
