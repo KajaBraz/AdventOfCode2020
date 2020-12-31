82e5ccf56174c7a9c8d4dd368d11b26a88f303e7
KajaBraz <kajabraz@gmail.com>
2020-12-31 01:47:57 +0100

    puzzle link added, typos removed

--- day21.py
+++ day21.py
@@ -1,3 +1,6 @@
+"""
+https://adventofcode.com/2020/day/21
+"""
 from day4 import get_list
 import re
 from collections import Counter
@@ -7,17 +10,17 @@ def parse_food_list(food: [str]) -> {(str, int): {str}}:
     dishes = {}
     count = 0
     for dish in food:
-        ingr_alerg = dish.split('contains')
-        ingredients = re.findall(r'\w+', ingr_alerg[0])
-        alergens = re.findall(r'\w+', ingr_alerg[1]) + [count]
-        dishes[tuple(alergens)] = set(ingredients)
+        ingr_allerg = dish.split('contains')
+        ingredients = re.findall(r'\w+', ingr_allerg[0])
+        allergens = re.findall(r'\w+', ingr_allerg[1]) + [count]
+        dishes[tuple(allergens)] = set(ingredients)
         count += 1
     return dishes
 
 
 def find_food_with_no_alergens(food: {(str): {str}}) -> [str]:
-    alergens = {alergen for multiple_alergens in food.keys() for alergen in multiple_alergens[:-1]}
-    ingredients_no_alergens = set().union(*food.values())
+    alergens = {alergen for multiple_allergens in food.keys() for alergen in multiple_allergens[:-1]}
+    ingredients_no_allergens = set().union(*food.values())
     pairs = []
     for alergen in alergens:
         possible_ingr = []
@@ -37,14 +40,14 @@ def find_food_with_no_alergens(food: {(str): {str}}) -> [str]:
                 ingredient = pair[1].pop()
                 found.append([pair[0], ingredient])
                 pairs.remove(pair)
-                if ingredient in ingredients_no_alergens:
-                    ingredients_no_alergens.remove(ingredient)
+                if ingredient in ingredients_no_allergens:
+                    ingredients_no_allergens.remove(ingredient)
                 for remaining in pairs:
                     if ingredient in remaining[1]:
                         remaining[1].remove(ingredient)
-    count_occurences = sum(
-        [Counter(dish)[ingredient] for dish in food.values() for ingredient in ingredients_no_alergens])
-    return found, count_occurences
+    count_occurrences = sum(
+        [Counter(dish)[ingredient] for dish in food.values() for ingredient in ingredients_no_allergens])
+    return found, count_occurrences
 
 
 def get_ingredients_list_sorted_by_allergen(pairs: [[str]]) -> str:
@@ -54,7 +57,7 @@ def get_ingredients_list_sorted_by_allergen(pairs: [[str]]) -> str:
 
 if __name__ == '__main__':
     dishes = parse_food_list(get_list('input21.txt'))
-    pairs_found, occurances_ingredients_no_alergens = find_food_with_no_alergens(dishes)  # part 1 solution
-    print(occurances_ingredients_no_alergens)
+    pairs_found, occurrences_ingredients_no_allergens = find_food_with_no_alergens(dishes)  # part 1 solution
+    print(occurrences_ingredients_no_allergens)
     ingredients_left = get_ingredients_list_sorted_by_allergen(pairs_found)  # part 2 solution
     print(ingredients_left)
